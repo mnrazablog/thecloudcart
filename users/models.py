864aeb573bf5499a657585262f198c59a887ee37
mnrazablog <mnraza94@gmail.com>
2019-11-26 18:16:22 +0530

    removed address from form and model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    address = models.CharField(max_length=400)
+    pass
 
     def __str__(self):
         return self.username
