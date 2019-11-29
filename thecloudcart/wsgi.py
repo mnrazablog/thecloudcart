4b397fc374cdbe5a46eedabca6fe83c88eeccad9
mnrazablog <mnraza94@gmail.com>
2019-11-29 17:42:40 +0530

    make it better

--- thecloudcart/wsgi.py
+++ thecloudcart/wsgi.py
@@ -6,7 +6,7 @@ import os
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thecloudcart.settings')
 
 application = get_wsgi_application()
 
