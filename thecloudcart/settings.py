522592c61f05584323e002ef5f03a53d512e6e10
mnrazablog <mnraza94@gmail.com>
2019-11-29 15:37:23 +0530

    changed wsgi file

--- thecloudcart/settings.py
+++ thecloudcart/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'registration',
+    'thecloudcart.registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
